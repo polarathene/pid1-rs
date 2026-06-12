64fe9b84952b04e77a7f025263e6cc946e297803
Brennan Kinney <5098581+polarathene@users.noreply.github.com>
2026-06-12 20:37:35 +1200

    chore: Indent to 4 spaces

--- pid1-exe/src/main.rs
+++ pid1-exe/src/main.rs
@@ -2,9 +2,9 @@
 mod cli;
 
 fn main() {
-  #[cfg(unix)]
-  cli::Pid1App::from_cli().run();
+    #[cfg(unix)]
+    cli::Pid1App::from_cli().run();
 
-  #[cfg(not(unix))]
-  compile_error!("`pid1` is only compatible with Unix-like operating systems.");
+    #[cfg(not(unix))]
+    compile_error!("`pid1` is only compatible with Unix-like operating systems.");
 }
